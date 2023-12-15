5bcc040086d5c09b0edb7a4b1259ee56b67eeb0a
Nguyen Van Quyen <153894626+quyen0505@users.noreply.github.com>
2023-12-15 14:52:27 +0700

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,20 @@
+package basic.dev;
+
+/**
+ * Nguyen Van Quyen 
+ * Syntax
+ */
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Hello World ");
+		int number;
+		String schoolname;
+	}
+
+	public void show() {
+	}
+
+}
+
